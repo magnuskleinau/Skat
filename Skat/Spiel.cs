92246d7bc4b7a4bdcc71c16819478c271e172ec7
jaidmin <j.laute3@googlemail.com>
2017-07-29 21:54:02 +0200

    commit

--- Skat/Spiel.cs
+++ Skat/Spiel.cs
@@ -4,17 +4,7 @@ namespace Skat
 {
     public enum Spieltyp
     {
-        FARBE, GRAND, NULL, RAMSCH
-    }
-
-    public enum Farbe
-    {
-        KREUZ = 12, PIK = 11, HERZ = 10, KARO = 9
-    }
-
-    public enum Spielstaerke
-    {
-        M1 = 2, M2 = 3, M3 = 4, M4 = 5, O1 = 2, O2 = 3, O3 = 4, O4 = 5
+        KREUZ, PIK , HERZ, KARO, GRAND, NULL, RAMSCH
     }
 
     public enum Ansage
@@ -24,18 +14,15 @@ namespace Skat
 
     public class Spiel
     {
-        //passed variables
         public Spieler spieler;
         public Spieler geber;
 
         public Guid id;
         public Guid abendId;
 
+        public DateTime datetime;
         public Spieltyp spieltyp;
-        public Farbe farbe;
-        public Spielstaerke spielstaerke;
-        public Ansage ansage;
-
+        
         public bool bock;
         public bool re;
         public bool kontra;
@@ -45,41 +32,13 @@ namespace Skat
         public int punkte;
         public int spielNummer;
 
-        //other variables
-        public DateTime datetime;
-
         public bool gewonnen;
 
-        public int spielwert;
 
-
-        public Spiel(Guid abendId, Guid id, int spielNummer, Spieler spieler, Spieler geber, Spieltyp spieltyp, Farbe farbe, Spielstaerke spielstaerke, Ansage ansage, bool bock, bool re, bool kontra, bool hand, bool ouvert, int punkte)
+        public Spiel(Guid abendId)
         {
-            //pass parameters to local variables
             this.abendId = abendId;
-            this.id = id;
-            this.spielNummer = spielNummer;
-            this.spieler = spieler;
-            this.geber = geber;
-            this.spieltyp = spieltyp;
-            this.farbe = farbe;
-            this.spielstaerke = spielstaerke;
-            this.ansage = ansage;
-            this.bock = bock;
-            this.re = re;
-            this.kontra = kontra;
-            this.hand = hand;
-            this.ouvert = ouvert;
-            this.punkte = punkte;
 
-            //init other variables
-            this.datetime = DateTime.Now;
-            calculateSpielwert();
-        }
-
-        private void calculateSpielwert()
-        {
-            spielwert = (int)farbe * (int)spielstaerke;
         }
 
         public Spiel(Guid abendId, Ansage ansage)
