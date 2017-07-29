9630dd93569b4e04826e31bb705fa264a3454658
magnuskleinau <magnus.kleinau@law-school.de>
2017-07-29 22:26:24 +0200

    new class Tests

---
+++ Skat/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skat
+{
+    class Tests
+    {
+        public Tests()
+        {
+            runTest();
+        }
+
+        private void runTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
